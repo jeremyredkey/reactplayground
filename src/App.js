f950005dd3e3e1ece5193b44ec178cb16b46424e
Jeremy Redkey <jeremy.redkey@gmail.com>
2020-08-21 01:03:09 -0700

    Replace src folder with new files and folders
    Replace src folder with new files and folders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import Split from './composition/Split.js';
-import { Button } from '@material-ui/core';
+import logo from './logo.svg';
+import HelloWorld from './state-drills/HelloWorld';
+import TheDate from './TheDate'
+import Counter from './state/Counter'
+import Bomb from './state-drills/Bomb'
+import RouletteGun from './state-drills/RouletteGun'
+import './App.css';
 
 function App() {
   return (
-    <main className='App'>
-<Split className="left">
-        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
-      </Split>
-      <Split className="right">
-        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-      <Button color="primary">Cool Button</Button>
-    </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <HelloWorld />
+        <TheDate />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Counter />
+        <Bomb />
+        <RouletteGun />
+      </header>
+    </div>
   );
 }
 
